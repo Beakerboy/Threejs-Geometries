aaabc7f0a4dca5db806bcbac9e4127a39f1c0a8b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2022-04-23 00:58:50 -0400

    Create WedgeGeometry.js

---
+++ src/WedgeGeometry.js
@@ -0,0 +1,54 @@
+import {
+	BufferGeometry,
+	Vector2,
+	Shape,
+	ShapeUtils,
+	BufferAttribute
+} from 'three';
+
+/**
+ * Modify ExtrudeGeometry such that z varies with x and y
+ */
+class WedgeGeometry extends BufferGeometry {
+  constructor(shape = new Shape( [ new Vector2( 0.5, 0.5 ), new Vector2( - 0.5, 0.5 ), new Vector2( - 0.5, - 0.5 ), new Vector2( 0.5, - 0.5 ) ] ), options = {}) {
+    super();
+
+    this.type = 'WedgeGeometry';
+
+    this.parameters = {
+      shape: shape,
+      options: options,
+    };
+
+    // The max depth of the geometry
+    var depth = options.depth;
+
+    // If depth is not specified, the angle of the ramp.
+    const pitch = options.pitch;
+
+    // The direction that the downward slope faces,
+    const angle = options.angle;
+
+    // Get the outer shape and holes.
+    var points = shape.extractPoints().shape;
+    var holes = shape.extractPoints().holes;
+
+    // Ensuse all paths are in the correct direction for the normals
+    const reverse = ! ShapeUtils.isClockWise( points );
+    if ( reverse ) {
+      points = points.reverse();
+      // Check that any holes are correct direction.
+      for (let h = 0; h < holes.length; h++) {
+        const hole = holes[h];
+        if (THREE.ShapeUtils.isClockWise(hole)) {
+          holes[h] = hole.reverse();
+        }
+      }
+    }
+    // Find the longest edge in the given angle.
+    // Divide that distance in half and find all outer and inner lines which cross
+    // a line perpendicular to the given angle.
+    // Create new shapes that are divided by the line, triangularize.
+  }
+}
+export { WedgeGeometry };
