41e8940fb851c2dac1275cf6871b716fe33a3261
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-19 08:31:16 -0400

    Update RampGeometry.js

--- src/RampGeometry.js
+++ src/RampGeometry.js
@@ -1,79 +1,92 @@
 import {
-  BufferGeometry,
-  Vector2,
-  Shape,
-  ShapeUtils,
-  BufferAttribute,
+	BufferGeometry,
+	Vector2,
+	Shape,
+	ShapeUtils,
+	BufferAttribute,
 } from 'three';
 
 /**
  * Modify ExtrudeGeometry such that z varies with x and y
  */
 class RampGeometry extends BufferGeometry {
-  constructor(shape = new Shape( [ new Vector2( 0.5, 0.5 ), new Vector2( - 0.5, 0.5 ), new Vector2( - 0.5, - 0.5 ), new Vector2( 0.5, - 0.5 ) ] ), options = {}) {
-    super();
 
-    this.type = 'RampGeometry';
+	constructor(shape = new Shape( [ new Vector2( 0.5, 0.5 ), new Vector2( - 0.5, 0.5 ), new Vector2( - 0.5, - 0.5 ), new Vector2( 0.5, - 0.5 ) ] ), options = {}) {
 
-    this.parameters = {
-      shape: shape,
-      options: options,
+		super();
+		this.type = 'RampGeometry';
+
+		this.parameters = {
+			
+			shape: shape,
+			options: options,
+
     };
 
-    // The max depth of the geometry
-    var depth = options.depth;
+		// The max depth of the geometry
+		var depth = options.depth;
 
-    // If depth is not specified, the angle of the ramp.
-    const pitch = options.pitch;
+		// If depth is not specified, the angle of the ramp.
+		const pitch = options.pitch;
 
-    // The direction that the downward slope faces,
-    const angle = options.angle;
+		// The direction that the downward slope faces,
+		const angle = options.angle;
 
-    // Get the outer shape and holes.
-    var points = shape.extractPoints().shape;
-    var holes = shape.extractPoints().holes;
+		// Get the outer shape and holes.
+		var points = shape.extractPoints().shape;
+		var holes = shape.extractPoints().holes;
 
-    // Ensuse all paths are in the correct direction for the normals
-    const reverse = ! ShapeUtils.isClockWise( points );
-    if ( reverse ) {
-      points = points.reverse();
-      // Check that any holes are correct direction.
-      for (let h = 0; h < holes.length; h++) {
-        const hole = holes[h];
-        if (ShapeUtils.isClockWise( hole )) {
-          holes[h] = hole.reverse();
+		// Ensuse all paths are in the correct direction for the normals
+		const reverse = ! ShapeUtils.isClockWise( points );
+		if ( reverse ) {
+			points = points.reverse();
+			// Check that any holes are correct direction.
+			for ( let h = 0; h < holes.length; h ++ ) {
+
+				const hole = holes[h];
+				if (ShapeUtils.isClockWise( hole )) {
+
+					holes[h] = hole.reverse();
+
         }
-      }
-    }
 
-    var rampDepth;
-    var nextRampDepth;
-    var minDepth;
-    var maxDepth;
-    var positions = [];
-    var point;
-    var nextPoint;
-    const vertices = [];
-    // Add the outer wall.
-    for (let i = 0; i < points.length - 1; i++) {
-      point = points[i];
-      vertices.push(point.x, point.y);
-      nextPoint = points[i + 1];
-      positions.push(point.x, point.y, 0);
-      rampDepth = point.x * Math.sin(angle) - point.y * Math.cos(angle);
-      nextRampDepth = nextPoint.x * Math.sin(angle) - nextPoint.y * Math.cos(angle);
-      if (i === 0) {
-        minDepth = rampDepth;
-        maxDepth = rampDepth;
-      } else {
-        minDepth = Math.min(rampDepth, minDepth);
-        maxDepth = Math.max(rampDepth, maxDepth);
       }
-      positions.push(point.x, point.y, rampDepth);
-      positions.push(nextPoint.x, nextPoint.y, 0);
-      positions.push(point.x, point.y, rampDepth);
-      positions.push(nextPoint.x, nextPoint.y, nextRampDepth);
-      positions.push(nextPoint.x, nextPoint.y, 0);
+
+		}
+
+		var rampDepth;
+		var nextRampDepth;
+		var minDepth;
+		var maxDepth;
+		var positions = [];
+		var point;
+		var nextPoint;
+		const vertices = [];
+		// Add the outer wall.
+		for ( let i = 0; i < points.length - 1; i ++ ) {
+
+			point = points[i];
+			vertices.push( point.x, point.y );
+			nextPoint = points[ i + 1 ];
+			positions.push( point.x, point.y, 0 );
+			rampDepth = point.x * Math.sin( angle ) - point.y * Math.cos( angle );
+			nextRampDepth = nextPoint.x * Math.sin(angle) - nextPoint.y * Math.cos(angle);
+			if ( i === 0 ) {
+
+				minDepth = rampDepth;
+				maxDepth = rampDepth;
+
+      } else {
+
+        minDepth = Math.min( rampDepth, minDepth );
+        maxDepth = Math.max( rampDepth, maxDepth );
+
+      }
+			positions.push( point.x, point.y, rampDepth );
+			positions.push( nextPoint.x, nextPoint.y, 0 );
+			positions.push( point.x, point.y, rampDepth );
+			positions.push( nextPoint.x, nextPoint.y, nextRampDepth );
+			positions.push( nextPoint.x, nextPoint.y, 0 );
     }
 
     // The highest and lowest points will be along the outside
