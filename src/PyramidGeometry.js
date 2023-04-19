0100ac63028bb9b1b003411ce4498b5ea35b7fc4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-19 08:13:50 -0400

    Update PyramidGeometry.js

--- src/PyramidGeometry.js
+++ src/PyramidGeometry.js
@@ -1,9 +1,9 @@
 import {
-		BufferGeometry,
-		Vector2,
-		Shape,
-		ShapeUtils,
-		BufferAttribute,
+	BufferGeometry,
+	Vector2,
+	Shape,
+	ShapeUtils,
+	BufferAttribute,
 } from 'three';
 
 class PyramidGeometry extends BufferGeometry {
@@ -25,27 +25,35 @@ class PyramidGeometry extends BufferGeometry {
 		if ( reverse ) {
 			points = points.reverse();
 		}
-		for (let i = 0; i < points.length - 1; i++) {
-			point = points[i];
-			nextPoint = points[i + 1];
-			positions.push(point.x, point.y, 0);
-			positions.push(center[0], center[1], depth);
-			positions.push(nextPoint.x, nextPoint.y, 0);
+
+		for ( let i = 0; i < points.length - 1; i ++ ) {
+			point = points[ i ];
+			nextPoint = points[ i + 1 ];
+			positions.push( point.x, point.y, 0 );
+			positions.push( center[0], center[1], depth );
+			positions.push( nextPoint.x, nextPoint.y, 0 );
 		}
+
 		// Add the floor
 		const faces = ShapeUtils.triangulateShape(points, []);
-		for (let i = 0; i < faces.length; i++) {
-			const face = faces[i];
-			for (let j = 2; j > -1; j--) {
-				const x = points[face[j]].x;
-				const y = points[face[j]].y;
+
+		for ( let i = 0; i < faces.length; i ++ ) {
+			const face = faces[ i ];
+
+			for ( let j = 2; j > -1; j -- ) {
+				const x = points[ face[ j ] ].x;
+				const y = points[ face[ j ] ].y;
 				const z = 0;
-				positions.push(x, y, z);
+				positions.push( x, y, z );
 			}
+
 		}
-		this.setAttribute('position', new BufferAttribute(new Float32Array(positions), 3));
+
+		this.setAttribute( 'position', new BufferAttribute( new Float32Array( positions ), 3 ) );
 		// ToDo - add points correctly so only one face needs to be rendered.
 		this.computeVertexNormals();
+
 	}
+
 }
 export { PyramidGeometry };
