e840b76d088f349c7d4838498147e188f86dbb49
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-04-24 10:26:07 -0400

    Update CustomObjectLoader.js

--- test/unit/CustomObjectLoader.js
+++ test/unit/CustomObjectLoader.js
@@ -1,7 +1,12 @@
-import * as Geometries from './Geometries.js';
 import { ObjectLoader } from 'three';
 
 class CustomObjectLoader extends ObjectLoader {
+  constructor( manager ) {
+
+		super( manager );
+    import * as Geometries from './Geometries.js';
+    
+	}
 }
 
 export { CustomObjectLoader };
