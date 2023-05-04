72795ade75053884e9c87dd509af888a7b9682d7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-05-04 10:08:25 -0400

    Update WedgeGeometry.js

--- src/WedgeGeometry.js
+++ src/WedgeGeometry.js
@@ -356,7 +356,6 @@ class WedgeGeometry extends BufferGeometry {
 	move( point ) {
 
 		const angle = this.parameters.options.angle;
-		const center = this.parameters.options.center;
 		const pointX = ( point.x - center[ 0 ] ) * Math.cos( angle ) - ( point.y - center[ 1 ] ) * Math.sin( angle );
 		const pointY = ( point.x - center[ 0 ] ) * Math.sin( angle ) + ( point.y - center[ 1 ] ) * Math.cos( angle );
 		return [ pointX, pointY ];
@@ -369,7 +368,6 @@ class WedgeGeometry extends BufferGeometry {
 	unMove( point ) {
 
 		const angle = this.parameters.options.angle;
-		const center = this.parameters.options.center;
 		const pointX = point[ 0 ] * Math.cos( angle ) + point[ 1 ] * Math.sin( angle ) + center[ 0 ];
 		const pointY = - 1 * point[ 0 ] * Math.sin( angle ) + point[ 1 ] * Math.cos( angle ) + center[ 1 ];
 		return [ pointX, pointY ];
