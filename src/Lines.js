5f598383442ce0194ade9fff9315ca930a001066
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-06-23 09:24:46 -0400

    Update Lines.js

--- src/Lines.js
+++ src/Lines.js
@@ -1,9 +1,18 @@
 class Line {
-// y = mx + b
+  // We have to allow vertical line
+  constructor(point, angle) {
+    this.point = point
+    this.angle = angle
+  }
 }
 
 class Ray extends Line {
-// point, slope, and direction
+// point: x-y point in space
+// angle: value between 0 and τ
+  constructor(point, angle) {
+    this.point = point
+    this.angle = angle
+  }
 }
 
 class LineSegment extends Line {
