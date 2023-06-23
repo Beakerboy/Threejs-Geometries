d75254c787454ea5a3655a32aa1b55f3503e8285
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-06-23 14:54:40 -0400

    Update Ray3.tests.js

--- test/unit/src/math/Ray3.tests.js
+++ test/unit/src/math/Ray3.tests.js
@@ -13,12 +13,12 @@ export default QUnit.module( 'Maths', () => {
 		QUnit.test( 'Instancing', ( assert ) => {
 
 			let a = new Ray();
-			assert.ok( a.origin.equals( zero3 ), 'Passed!' );
-			assert.ok( a.direction.equals( new Vector3( 0, 0, - 1 ) ), 'Passed!' );
+			assert.ok( a.origin.equals( zero2 ), 'Passed!' );
+			assert.ok( a.direction.equals( new Vector2( 0, - 1 ) ), 'Passed!' );
 
-			a = new Ray( two3.clone(), one3.clone() );
-			assert.ok( a.origin.equals( two3 ), 'Passed!' );
-			assert.ok( a.direction.equals( one3 ), 'Passed!' );
+			a = new Ray( two2.clone(), one2.clone() );
+			assert.ok( a.origin.equals( two2 ), 'Passed!' );
+			assert.ok( a.direction.equals( one2 ), 'Passed!' );
 
 		} );
     
