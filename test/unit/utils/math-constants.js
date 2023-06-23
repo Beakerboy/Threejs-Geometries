43dd69cc146a3220dd3c6d3b7ec209dffea0891b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-06-23 14:53:29 -0400

    Create math-constants.js

---
+++ test/unit/utils/math-constants.js
@@ -0,0 +1,18 @@
+import { Vector2 } from '../../../src/math/Vector2.js';
+
+export const x = 2;
+export const y = 3;
+export const z = 4;
+export const w = 5;
+
+export const negInf2 = new Vector2( - Infinity, - Infinity );
+export const posInf2 = new Vector2( Infinity, Infinity );
+
+
+export const negOne2 = new Vector2( - 1, - 1 );
+
+export const zero2 = new Vector2();
+export const one2 = new Vector2( 1, 1 );
+export const two2 = new Vector2( 2, 2 );
+
+export const eps = 0.0001;
