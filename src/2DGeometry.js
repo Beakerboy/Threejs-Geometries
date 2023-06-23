eeeded08f0e376489d8ed032299c5039fb50b543
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-06-23 09:52:53 -0400

    Update 2DGeometry.js

--- src/2DGeometry.js
+++ src/2DGeometry.js
@@ -4,23 +4,57 @@ class Point {
 class Vector {
 }
 
-class Line {
-  // We have to allow vertical line
-  constructor(point, vector) {
-    this.point = point
-    this.vector = vector
-  }
+class Line2 {
+  constructor( start = new Vector2(), end = new Vector2() ) {
+
+		this.start = start;
+		this.end = end;
+
+	}
 }
 
-class Ray extends Line {
+class Ray2 {
   // point: x-y point in space
   // vector: 
-  constructor(point, vector) {
-    this.point = point
-    this.vector = vector
-  }
+  constructor( origin = new Vector2(), direction = new Vector2( 0, 0, - 1 ) ) {
+
+		this.origin = origin;
+		this.direction = direction;
+
+	}
+
+  set( origin, direction ) {
+
+		this.origin.copy( origin );
+		this.direction.copy( direction );
+
+		return this;
+
+	}
+
+	copy( ray ) {
+
+		this.origin.copy( ray.origin );
+		this.direction.copy( ray.direction );
+
+		return this;
+
+	}
+
+  equals( ray ) {
+
+		return ray.origin.equals( this.origin ) && ray.direction.equals( this.direction );
+
+	}
+
+	clone() {
+
+		return new this.constructor().copy( this );
+
+	}
+
 }
 
 class LineSegment extends Line {
-// Two Points
+
 }
