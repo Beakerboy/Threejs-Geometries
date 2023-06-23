2e0d07a3aeabffc4cdedd5c5e9289152237cade5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-06-23 10:08:33 -0400

    Update Line2.js

--- src/math/Line2.js
+++ src/math/Line2.js
@@ -1,8 +1,21 @@
 class Line2 {
-  constructor( start = new Vector2(), end = new Vector2() ) {
+	constructor( start = new Vector2(), end = new Vector2() ) {
 
 		this.start = start;
 		this.end = end;
 
 	}
+
+	equals( line ) {
+
+		return line.start.equals( this.start ) && line.end.equals( this.end );
+
+	}
+
+	clone() {
+
+		return new this.constructor().copy( this );
+
+	}
+
 }
