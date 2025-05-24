74a58c1a41f7ae5b4c2bf3bc4cba6ffbfa5b68ad
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-24 14:55:04 -0400

    Update HippedGeometry.js

--- src/HippedGeometry.js
+++ src/HippedGeometry.js
@@ -4,8 +4,6 @@ import {
 	Shape,
 } from 'three';
 
-import SkeletonBuilder from 'straight-skeleton';
-
 class HippedGeometry extends BufferGeometry {
 
 	constructor( shape = new Shape( [ new Vector2( 0.5, 0.5 ), new Vector2( - 0.5, 0.5 ), new Vector2( - 0.5, - 0.5 ), new Vector2( 0.5, - 0.5 ) ] ), options = {} ) {
