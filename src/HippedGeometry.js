dbf35715768ff9f614f14d620926d18110e262a3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-28 09:09:11 -0400

    Update HippedGeometry.js

--- src/HippedGeometry.js
+++ src/HippedGeometry.js
@@ -36,24 +36,28 @@ class HippedGeometry extends BufferGeometry {
 				const geometry = new THREE.BufferGeometry();
 				const vertices = [];
 
-				for (const polygon of result.polygons) {
+				for ( const polygon of result.polygons ) {
+
 					const polygonVertices = [];
-					for (let i = 0; i < polygon.length; i ++) {
-						const vertex = activeSkeleton.vertices[polygon[i]];
+					for ( let i = 0; i < polygon.length; i ++ ) {
+
+						const vertex = activeSkeleton.vertices[ polygon[ i ] ];
 						polygonVertices.push(
-							(vertex[0] + offset.x) * scale,
-							(vertex[1] + offset.y) * scale,
-							(vertex[2] + offset.z) * scale
+							(vertex[ 0 ] + offset.x) * scale,
+							(vertex[ 1 ] + offset.y) * scale,
+							(vertex[ 2 ] + offset.z) * scale
 						);
+
 					}
-					const triangles = earcut(polygonVertices, null, 3);
+
+					const triangles = earcut( polygonVertices, null, 3 );
 
 					for ( let i = 0; i < triangles.length / 3; i ++ ) {
 
 						for ( let j = 0; j < 3; j ++ ) {
 
 							const index = triangles[ i * 3 + j ];
-							vertices.push(polygonVertices[ index * 3 ], polygonVertices[ index * 3 + 1 ], polygonVertices[ index * 3 + 2 ]);
+							vertices.push( polygonVertices[ index * 3 ], polygonVertices[ index * 3 + 1 ], polygonVertices[ index * 3 + 2 ] );
 
 						}
 
@@ -63,7 +67,9 @@ class HippedGeometry extends BufferGeometry {
 
 				this.setAttribute( 'position', new BufferAttribute( new Float32Array( vertices ), 3 ) );
 				this.computeVertexNormals();
+
 			}
+
 		} );
 
 	}
