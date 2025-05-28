a925bab5d26d00b0824928d0bb75b76ab3479989
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-28 08:54:04 -0400

    Update HippedGeometry.js

--- src/HippedGeometry.js
+++ src/HippedGeometry.js
@@ -3,6 +3,7 @@ import {
 	Vector2,
 	Shape,
 } from 'three';
+import earcut from "earcut";
 import { SkeletonBuilder } from 'straight-skeleton';
 
 class HippedGeometry extends BufferGeometry {
@@ -16,6 +17,45 @@ class HippedGeometry extends BufferGeometry {
 			options: options,
 		};
 
+		const polygon = [
+			[
+				[-1, -1], [0, -12], [1, -1], [12, 0], [1, 1], [0, 12], [-1, 1], [-12, 0], [-1, -1]
+			],
+			[
+				[-1, 0],  [0, 1], [0, -1], [-1, 0]
+			]
+		];
+
+		// Initialize the Wasm module by calling init() once.
+		SkeletonBuilder.init().then(() => {
+			const result = SkeletonBuilder.buildFromPolygon(polygon);
+	
+			// Check if the skeleton was successfully constructed
+			if (result !== null) {
+				const geometry = new THREE.BufferGeometry();
+				const vertices: number[] = [];
+
+				for (const polygon of result.polygons) {
+					const polygonVertices: number[] = [];
+					for (let i = 0; i < polygon.length; i++) {
+						const vertex = activeSkeleton.vertices[polygon[i]];
+						polygonVertices.push(
+							(vertex[0] + offset.x) * scale,
+							(vertex[1] + offset.y) * scale,
+							(vertex[2] + offset.z) * scale
+						);
+					}
+					const triangles = earcut(polygonVertices, null, 3);
+					for (let i = 0; i < triangles.length / 3; i++) {
+						for (let j = 0; j < 3; j++) {
+							const index = triangles[i * 3 + j];
+							vertices.push(polygonVertices[index * 3], polygonVertices[index * 3 + 1], polygonVertices[index * 3 + 2]);
+						}
+					}
+				}
+			}
+			geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
+		});
 	}
 
 	static fromJSON( data, shape ) {
