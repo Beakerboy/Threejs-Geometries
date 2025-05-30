caa629bbca353b0bb33903de1ef29c5c65ec1771
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-30 13:17:21 -0400

    Added Hipped Roof (#15)
    * Added HippedRoof class
    * Added 100% code coverage in tests
    * Added example pages to display control via depth and pitch options

---
+++ src/HippedGeometry.js
@@ -0,0 +1,155 @@
+import {
+	BufferGeometry,
+	BufferAttribute,
+	Vector2,
+	Shape,
+	ShapeUtils,
+} from 'three';
+import { SkeletonBuilder } from 'straight-skeleton';
+
+class HippedGeometry extends BufferGeometry {
+
+	constructor( shape = new Shape( [ new Vector2( 0.5, 0.5 ), new Vector2( - 0.5, 0.5 ), new Vector2( - 0.5, - 0.5 ), new Vector2( 0.5, - 0.5 ) ] ), options = {} ) {
+
+		super();
+		this.type = 'HippedGeometry';
+		this.parameters = {
+			shape: shape,
+			options: options,
+		};
+		/** {number} The max depth of the geometry */
+		var depth = options.depth;
+
+		/** {number} The roof angle in radians */
+		const pitch = options.pitch;
+
+		var points = shape.extractPoints().shape;
+		var holes = shape.extractPoints().holes;
+
+		// {number[]} Flat array of x, y, z tuples
+		const polygonVertices = [];
+
+		// straight-skeleton expects counter-clockwise outer polygon
+		if ( ShapeUtils.isClockWise( points ) ) {
+
+			points.reverse();
+
+		}
+
+		for ( const hole of holes ) {
+
+			// straight-skeleton expects clockwise inner polygon
+			if ( ! ShapeUtils.isClockWise( hole ) ) {
+
+				hole.reverse();
+
+			}
+
+		}
+
+		/**
+                 * A multipolygon is an array of polygons. Each polygon is an array of one or more
+                 * closed shapes. The first shape is the outer perimeter. Subsequent shapes are
+                 * holes.
+		 */
+		// {[number, number][]} An array of x, y points
+		const border = [];
+		// {border[]} An array of borders. Element 0 is an outer, and additional borders are inner.
+		const polygon = [];
+		// {polygon[]} all polygons in the multipolygon.
+		const multipolygon = [];
+
+		for ( const point of points ) {
+
+			border.push( [ point.x, point.y ] );
+
+		}
+
+		// despite GeoJSON requiring repeated start and end points,
+		// the library does not like the repeated point.
+		polygon.push( border );
+		for ( const hole of holes ) {
+
+			const holeBorder = [];
+			for ( const point of hole ) {
+
+				holeBorder.push( [ point.x, point.y ] );
+
+			}
+
+			polygon.push( holeBorder );
+
+		}
+
+		multipolygon.push( polygon );
+		const result = SkeletonBuilder.BuildFromGeoJSON( multipolygon );
+
+		// Check if the skeleton was successfully constructed
+		if ( result !== null ) {
+
+			const maxDepth = Math.max( ...result.Distances.values() );
+			const scalingFactor = pitch === undefined ? ( depth === undefined ? 1 : depth / maxDepth ) : Math.tan( pitch );
+
+			// Create an array of Vector2 for each face
+			for ( const edgeOutput of result.Edges ) {
+
+				const newPolygon = [];
+				const heights = [];
+				// convert List of Vector2d to array of Vector2
+				for ( const point of edgeOutput.Polygon ) {
+
+					newPolygon.push( new Vector2( point.X, point.Y ) );
+					heights.push( result.Distances.get( point ) * scalingFactor );
+
+				}
+
+				// Triangulate the upper surface
+				// {[number, number, number][]}
+				const upperTriangles = ShapeUtils.triangulateShape( newPolygon, [] );
+
+				for ( const triangle of upperTriangles ) {
+
+					polygonVertices.push( newPolygon[ triangle[ 0 ] ].x, newPolygon[ triangle[ 0 ] ].y, heights[ triangle[ 0 ] ] );
+					polygonVertices.push( newPolygon[ triangle[ 1 ] ].x, newPolygon[ triangle[ 1 ] ].y, heights[ triangle[ 1 ] ] );
+					polygonVertices.push( newPolygon[ triangle[ 2 ] ].x, newPolygon[ triangle[ 2 ] ].y, heights[ triangle[ 2 ] ] );
+
+				}
+
+			}
+
+			// Triangulate the bottom
+			/** @type {[number, number, number][]} */
+			const bottomTriangles = ShapeUtils.triangulateShape( points, holes );
+			/** @type {Vector2[]} */
+			const allPoints = [];
+			allPoints.push( ...points );
+
+			for ( const hole of holes ) {
+
+				allPoints.push( ...hole );
+
+			}
+
+			for ( const triangle of bottomTriangles ) {
+
+				polygonVertices.push( allPoints[ triangle[ 0 ] ].x, allPoints[ triangle[ 0 ] ].y, 0 );
+				polygonVertices.push( allPoints[ triangle[ 1 ] ].x, allPoints[ triangle[ 1 ] ].y, 0 );
+				polygonVertices.push( allPoints[ triangle[ 2 ] ].x, allPoints[ triangle[ 2 ] ].y, 0 );
+
+			}
+
+			this.setAttribute( 'position', new BufferAttribute( new Float32Array( polygonVertices ), 3 ) );
+			this.computeVertexNormals();
+
+		}
+
+	}
+
+	static fromJSON( data, shape ) {
+
+		return new HippedGeometry( shape, data.options );
+
+	}
+
+}
+export { HippedGeometry };
---
+++ test/unit/src/geometries/HippedGeometry.tests.js
@@ -0,0 +1,121 @@
+/* global QUnit */
+
+import { HippedGeometry } from '../../../../src/HippedGeometry.js';
+
+import { BufferGeometry, Shape } from 'three';
+import { runStdGeometryTests } from '../../utils/qunit-utils.js';
+
+export default QUnit.module( 'Geometries', () => {
+
+	QUnit.module( 'HippedGeometry', ( hooks ) => {
+
+		let geometries = undefined;
+		hooks.beforeEach( function () {
+
+			const x = 0, y = 0;
+
+			const square = new Shape();
+			square.moveTo( - 25, - 25 );
+			square.lineTo( 75, - 25 );
+			square.lineTo( 75, 25 );
+			square.lineTo( - 25, 25 );
+
+			const doughnut = new Shape();
+			doughnut.moveTo( - 25, - 25 );
+			doughnut.lineTo( - 25, 25 );
+			doughnut.lineTo( 25, 25 );
+			doughnut.lineTo( 25, - 25 );
+
+			const innerShape = new Shape();
+			innerShape.moveTo( - 5, - 5 );
+			innerShape.lineTo( 5, - 5 );
+			innerShape.lineTo( 5, 5 );
+			innerShape.lineTo( - 5, 5 );
+
+			doughnut.holes.push( innerShape );
+			const options = {
+				depth: 6,
+			};
+
+			const options2 = {
+				pitch: 26.57 / 180 * Math.PI,
+			};
+
+			geometries = [
+				new HippedGeometry( square, options2 ),
+				new HippedGeometry( square ),
+				new HippedGeometry( square, options ),
+				new HippedGeometry( doughnut )
+			];
+
+		} );
+
+		// Data
+		QUnit.test( 'Data', ( assert ) => {
+
+			const facePoints = geometries[ 1 ].getAttribute( "position" );
+			assert.equal( facePoints.count, 24, "HippedGeometry Point Count:" );
+			assert.equal( facePoints.array.length, 72, "HippedGeometry Coordinate Count:" );
+			// uncomment below to peek at contents
+			// assert.equal( facePoints, [], "Contents of position array" );
+			assert.equal( Math.max( ...facePoints.array.filter( ( element, index ) => ( index + 1 ) % 3 === 0 ) ), 25, "Height should be 25" );
+
+			assert.equal( Math.max( ...geometries[ 2 ].getAttribute( "position" ).array.filter( ( element, index ) => ( index + 1 ) % 3 === 0 ) ), 6, "Height should be 6" );
+			assert.equal( Math.max( ...geometries[ 0 ].getAttribute( "position" ).array.filter( ( element, index ) => ( index + 1 ) % 3 === 0 ) ), 12.50269889831543, "Height should be about 12.5" );
+			assert.equal( geometries[ 3 ].getAttribute( "position" ).count, 72, "HippedGeometry Point Count with hole" );
+
+		} );
+
+		// INHERITANCE
+		QUnit.test( 'Extending', ( assert ) => {
+
+			const object = new HippedGeometry();
+			assert.strictEqual(
+				object instanceof BufferGeometry, true,
+				'HippedGeometry extends from BufferGeometry'
+			);
+
+		} );
+
+		// INSTANCING
+		QUnit.test( 'Instancing', ( assert ) => {
+
+			const object = new HippedGeometry();
+			assert.ok( object, 'Can instantiate a HippedGeometry.' );
+
+		} );
+
+		// PROPERTIES
+		QUnit.test( 'type', ( assert ) => {
+
+			const object = new HippedGeometry();
+			assert.ok(
+				object.type === 'HippedGeometry',
+				'HippedGeometry.type should be HippedGeometry'
+			);
+
+		} );
+
+		QUnit.todo( 'parameters', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// STATIC
+		QUnit.todo( 'fromJSON', ( assert ) => {
+
+			assert.ok( false, 'everything\'s gonna be alright' );
+
+		} );
+
+		// OTHERS
+		QUnit.test( 'Standard geometry tests', ( assert ) => {
+
+			runStdGeometryTests( assert, geometries );
+
+		} );
+
+	} );
+
+} );
