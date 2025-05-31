f9b6e95e5c48fdc2c143b619212a13f86e989eba
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-05-30 21:08:56 -0400

    Update PyramidGeometry.tests.js

--- test/unit/src/geometries/PyramidGeometry.tests.js
+++ test/unit/src/geometries/PyramidGeometry.tests.js
@@ -12,13 +12,18 @@ export default QUnit.module( 'Geometries', () => {
 		let geometries = undefined;
 		hooks.beforeEach( function () {
 
-			const square = new Shape();
+			const badSquare = new Shape();
+			badSquare.moveTo( - 5, - 5 );
+			badSquare.lineTo( 5, - 5 );
+			badSquare.lineTo( 5, 5 );
+			badSquare.lineTo( - 5, 5 );
+			badSquare.lineTo( - 5, - 5 );
 
+			const square = new Shape();
 			square.moveTo( - 5, - 5 );
 			square.lineTo( 5, - 5 );
 			square.lineTo( 5, 5 );
 			square.lineTo( - 5, 5 );
-			square.lineTo( - 5, - 5 );
 
 			const options = {
 				center: [ 0, 0 ],
@@ -27,21 +32,23 @@ export default QUnit.module( 'Geometries', () => {
 
 			geometries = [
 				new PyramidGeometry(),
-				new PyramidGeometry( square ),
 				new PyramidGeometry( square, options ),
+				new PyramidGeometry( badSquare, options ),
 			];
 
 		} );
 		// Data
 		QUnit.test( 'Data', ( assert ) => {
 
-			const facePoints = geometries[ 2 ].getAttribute( "position" );
-			assert.equal( facePoints.count, 18, "PyramidGeometry Point Count:" );
-			assert.equal( facePoints.array.length, 54, "PyramidGeometry Coordinate Count:" );
+			assert.equal( geometries[ 2 ].getAttribute( "position" ).count, 18, "PyramidGeometry Point Count:" );
+			assert.equal( geometries[ 2 ].getAttribute( "position" ).array.length, 54, "PyramidGeometry Coordinate Count:" );
 			// uncomment below to peek at contents
 			// assert.equal( facePoints, [], "Contents of position array" );
 			assert.equal( Math.max( ...facePoints.array.filter( ( element, index ) => ( index + 1 ) % 3 === 0 ) ), 5, "Height should be 5" );
 
+			assert.equal( geometries[ 1 ].getAttribute( "position" ).count, 18, "PyramidGeometry Correct Point Count:" );
+			assert.equal( geometries[ 1 ].getAttribute( "position" ).array.length, 54, "PyramidGeometry Correct Coordinate Count:" );
+			
 		} );
 
 		// INHERITANCE
