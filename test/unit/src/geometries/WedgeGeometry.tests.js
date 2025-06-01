661dfe972981a84daffd53e77983810de0cc8af5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-01 13:49:04 -0400

    Update WedgeGeometry.tests.js

--- test/unit/src/geometries/WedgeGeometry.tests.js
+++ test/unit/src/geometries/WedgeGeometry.tests.js
@@ -73,6 +73,7 @@ export default QUnit.module( 'Geometries', () => {
 
 			assert.equal( geometries[ 2 ].getAttribute( "position" ).count, 36, "WedgeGeometry correct Point Count" );
 			assert.equal( geometries[ 2 ].parameters.shape.extractPoints().shape.length, 6, "WedgeGeometry shape points should be 6" );
+			assert.equal( geometries[ 2 ].newShapes.length, 3, "number of shapes" );
 
 		} );
 
