a41f56cccc32cf5294e193f638293ed71c23ff8b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-02 11:55:14 -0400

    Update WedgeGeometry.tests.js

--- test/unit/src/geometries/WedgeGeometry.tests.js
+++ test/unit/src/geometries/WedgeGeometry.tests.js
@@ -9,59 +9,6 @@ export default QUnit.module( 'Geometries', () => {
 
 	QUnit.module( 'WedgeGeometry', ( hooks ) => {
 
-		let geometries = undefined;
-		hooks.beforeEach( function () {
-
-			const x = 0, y = 0;
-
-			const heartShape = new Shape();
-
-			heartShape.moveTo( x + 5, y + 5 );
-			heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
-			heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7, x - 6, y + 7 );
-			heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
-			heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
-			heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
-			heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
-
-			const rectangle = new Shape();
-			rectangle.moveTo( - 2, - 1 );
-			rectangle.lineTo( 2, - 1 );
-			rectangle.lineTo( 2, 1 );
-			rectangle.lineTo( - 2, 1 );
-			rectangle.lineTo( - 2, - 1 );
-
-			// Clockwise square
-			const square = new Shape();
-			square.moveTo( - .5, .5 );
-			square.lineTo( .5, .5 );
-			square.lineTo( .5, - .5 );
-			square.lineTo( - .5, - .5 );
-			square.lineTo( - .5, .5 );
-			// rectangle.holes.push( square );
-
-			const goodRectangle = new Shape();
-			goodRectangle.moveTo( - 2, - 1 );
-			goodRectangle.lineTo( - 2, 1 );
-			goodRectangle.lineTo( 2, 1 );
-			goodRectangle.lineTo( 2, - 1 );
-
-			const options = {
-				angle: 0,
-				depth: 5,
-				center: [ 0, 0 ],
-			};
-
-			geometries = [
-				new WedgeGeometry(),
-				new WedgeGeometry( heartShape ),
-				new WedgeGeometry( goodRectangle, options ),
-				// Outer shape in wrong direction, with hole in wrong direction.
-				new WedgeGeometry( rectangle, options ),
-			];
-
-		} );
-
 		// Data
 		QUnit.test( 'Data', ( assert ) => {
 
@@ -101,46 +48,6 @@ export default QUnit.module( 'Geometries', () => {
 			assert.equal( result.crossings[ 0 ], - 2, "First Crossing" );
 			assert.equal( result.crossings[ 3 ], 2, "Second Crossing" );
 			assert.equal( Object.keys( result.crossings ).length, 2, "There should be only two crossings" );
-
-		} );
-
-		// INHERITANCE
-		QUnit.test( 'Extending', ( assert ) => {
-
-			const object = new WedgeGeometry();
-			assert.strictEqual(
-				object instanceof BufferGeometry, true,
-				'WedgeGeometry extends from BufferGeometry'
-			);
-
-		} );
-
-		// INSTANCING
-		QUnit.test( 'Instancing', ( assert ) => {
-
-			const object = new WedgeGeometry();
-			assert.ok( object, 'Can instantiate a WedgeGeometry.' );
-
-		} );
-
-		// PROPERTIES
-		QUnit.test( 'type', ( assert ) => {
-
-			const object = new WedgeGeometry();
-			assert.ok(
-				object.type === 'WedgeGeometry',
-				'WedgeGeometry.type should be WedgeGeometry'
-			);
-
-		} );
-
-		// OTHERS
-		QUnit.test( 'Standard geometry tests', ( assert ) => {
-
-			runStdGeometryTests( assert, geometries );
-			const geo = geometries[ 2 ];
-			geo.computeBoundingSphere();
-			assert.ok( geo.boundingSphere.radius );
 
 		} );
 
