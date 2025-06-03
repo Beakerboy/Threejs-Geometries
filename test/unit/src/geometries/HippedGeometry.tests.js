1c68577589ffc3a5c948f0c0d5fabc437b458d54
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-06-03 17:34:16 -0400

    Update HippedGeometry.tests.js

--- test/unit/src/geometries/HippedGeometry.tests.js
+++ test/unit/src/geometries/HippedGeometry.tests.js
@@ -2,7 +2,7 @@
 
 import { HippedGeometry } from '../../../../src/HippedGeometry.js';
 
-import { BufferGeometry, Shape } from 'three';
+import { BufferGeometry, Shape, Vector2 } from 'three';
 import { runStdGeometryTests } from '../../utils/qunit-utils.js';
 
 export default QUnit.module( 'Geometries', () => {
@@ -63,6 +63,15 @@ export default QUnit.module( 'Geometries', () => {
 			assert.equal( Math.max( ...geometries[ 2 ].getAttribute( "position" ).array.filter( ( element, index ) => ( index + 1 ) % 3 === 0 ) ), 6, "Height should be 6" );
 			assert.equal( Math.max( ...geometries[ 0 ].getAttribute( "position" ).array.filter( ( element, index ) => ( index + 1 ) % 3 === 0 ) ), 12.50269889831543, "Height should be about 12.5" );
 			assert.equal( geometries[ 3 ].getAttribute( "position" ).count, 72, "HippedGeometry Point Count with hole" );
+
+		} );
+
+		// Data
+		QUnit.test( 'Bug', ( assert ) => {
+
+			const points = [ [ - 2, - 1 ], [ - 2, 0 ], [ - 2, 2 ], [ 3, 2 ], [ 3, - 1 ], [ 2, - 1 ], [0, - 1 ] ];
+			const shape = new Shape( points.map( point => new Vector2( ...point ) ) );
+			const geometry = new HippedGeometry( shape, options );
 
 		} );
 
